9c943220b256b7347e300234bfebe76b060ec6f3
meghanabaddipudireddy <baddipudim829@gmail.com>
2025-09-03 00:08:53 +0000

    lol

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
