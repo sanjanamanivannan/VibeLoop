25a40b05ffdc10acd002d9f116f7ee435f2a3946
sanjanamanivannan <sanjanamanivannan14@gmail.com>
2025-09-03 21:42:10 -0700

    save local changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import React, { useState, useEffect } from "react";
 
 import LoginWithSpotify from "./LoginWithSpotify";
 import Callback from "./Callback";
